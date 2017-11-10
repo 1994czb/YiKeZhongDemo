c872aaceb7734342b0a322dd24ce8d36483398d2
caozhongbin <861088350@qq.com>
2017-11-10 16:21:40 +0800

    实现了标题栏的效果！以及图片的点击事件，略微改动了布局中的图片

--- app/src/main/java/com/yikezhong/demo/activity/MainActivity.java
+++ app/src/main/java/com/yikezhong/demo/activity/MainActivity.java
@@ -1,5 +1,6 @@
-package com.yikezhong.demo;
+package com.yikezhong.demo.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.design.widget.NavigationView;
@@ -8,14 +9,18 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
+import com.yikezhong.demo.R;
 import com.yikezhong.demo.adapter.MyViewpagerAdapter;
 import com.yikezhong.demo.custom.MyViewPager;
+import com.yikezhong.demo.fragment.Cross_talkFragment;
 import com.yikezhong.demo.fragment.VideoFragment;
 import com.yikezhong.demo.fragment.recommend.RecommendFragment;
-import com.yikezhong.demo.fragment.Cross_talkFragment;
 
 import java.util.ArrayList;
 
@@ -27,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private ArrayList<Fragment> fragments;
     private RadioButton rb_fg_punch;
+
+    //标题栏
+    private ImageView image_left;
+    private TextView title;
+    private ImageView image_right;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,21 @@ public class MainActivity extends AppCompatActivity {
         //设置适配器
         viewpager.setAdapter(new MyViewpagerAdapter(getSupportFragmentManager(), fragments, this));
         radioFragment();
+        //点击左侧图片打开侧滑菜单
+        image_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
+        //点击右侧图片跳转到创作页面
+        image_right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,CrationActivity.class));
+            }
+        });
 
     }
 
@@ -54,12 +80,15 @@ public class MainActivity extends AppCompatActivity {
                 switch (checkedId) {
                     case R.id.rb_fg_punch:
                         current = 0;
+                        title.setText("推荐");
                         break;
                     case R.id.rb_fg_report:
                         current = 1;
+                        title.setText("段子");
                         break;
                     case R.id.rb_fg_msg:
                         current = 2;
+                        title.setText("视频");
                         break;
                 }
                 if (viewpager.getCurrentItem() != current) {
@@ -101,14 +130,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     //找控件
     private void initView() {
+        image_right = (ImageView) findViewById(R.id.icon_right);
+        image_left = (ImageView) findViewById(R.id.icon_left);
+        title = (TextView) findViewById(R.id.title);
         viewpager = (MyViewPager) findViewById(R.id.viewPager);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         rb_fg_punch = (RadioButton) findViewById(R.id.rb_fg_punch);
+
         fragments = new ArrayList<>();
         fragments.add(new RecommendFragment());
         fragments.add(new Cross_talkFragment());
         fragments.add(new VideoFragment());
+        title.setText("推荐");
     }
 }
